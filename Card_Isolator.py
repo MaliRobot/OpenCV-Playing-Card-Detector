b18606a11abc2eb99259aca9eb6064689136eff9
Milos Trifunovic <milos.trifunovic@gmail.com>
2018-02-08 23:32:29 +0100

    isolate both black and white

--- Card_Isolator.py
+++ Card_Isolator.py
@@ -51,24 +51,21 @@ if PiOrUSB == 2:
 # Use counter variable to switch from isolating Rank to isolating Suit
 i = 1
 
-#for Name in ['reito_lantern','ornate_kanzashi', 'free_from_the_real', 
-#              'sakura_tribe_scout', 'plains_ben_thomposon', 'path_of_angers_flame', 
-#              'sift_through_sands', 'setons_desire', 'phantom_nomad', 
-#              'divine_light', 'ghostly_wings', 'plains_fred_fields', 'locust_mister',
-#              'jugan_the_rising_star', 'whispering_shade', 'divergent_growth', 
-#              'ryusei_the_falling_star', 'dripping_tongue_zubera',
-#              'ninja_of_the _deep_hours', 'plains_matthew_mitchell', 'plains_greg_staples',
-#              'forest_quinton_hoover', 'forest_john_avon']:
-    
-for Name in ['white']:
+for Name in ['reito_lantern','ornate_kanzashi', 'free_from_the_real', 
+              'sakura_tribe_scout', 'plains_ben_thomposon', 'path_of_angers_flame', 
+              'sift_through_sands', 'setons_desire', 'phantom_nomad', 
+              'divine_light', 'ghostly_wings', 'plains_fred_fields', 'locust_mister',
+              'jugan_the_rising_star', 'whispering_shade', 'divergent_growth', 
+              'ryusei_the_falling_star', 'dripping_tongue_zubera',
+              'ninja_of_the _deep_hours', 'plains_matthew_mitchell', 'plains_greg_staples',
+              'forest_quinton_hoover', 'forest_john_avon']:
 
     filename = Name + '.jpg'
+    card = None
     
     while True:
         print('Press "p" to take a picture of ' + filename)
         
-        
-    
         if PiOrUSB == 1: # PiCamerac
             rawCapture.truncate(0)
             # Press 'p' to take a picture
@@ -96,7 +93,6 @@ for Name in ['white']:
                 except Exception as e:
                     print(e)
                 
-
         try:
             # Pre-process image
             thresh = Cards.preprocess_image(image)
@@ -106,20 +102,23 @@ for Name in ['white']:
             
             # Find contours and sort them by size
             dummy,cnts,hier = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-#            dummy,cnts,hier = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            dummy,cnts_wht,hier2 = cv2.findContours(thresh_wb,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
             cnts = sorted(cnts, key=cv2.contourArea,reverse=True)
-#            cnts = [x for x in cnts if cv2.contourArea(x) > 50000]
+            cnts_wht = sorted(cnts_wht, key=cv2.contourArea,reverse=True)
         
             # Assume largest contour is the card. If there are no contours, print an error
             flag = 0
             image2 = image.copy()
-        
-            if len(cnts) == 0:
+            
+            # Decide is it card with white or black background                
+            if len(cnts) > 0:
+                card = cnts[0]
+            elif len(cnts_wht) > 0:
+                card = cnts_wht[0]
+            else:
                 print('No contours found!')
                 quit()
-        
-            card = cnts[0]
         
             # Approximate the corner points of the card
             peri = cv2.arcLength(card,True)
@@ -131,16 +130,6 @@ for Name in ['white']:
             # Flatten the card and convert it to 200x300
             warp = Cards.flattener(image,pts,w,h)
             if debug_pics: cv2.imwrite(debug_path + "1_rectangle.jpg",warp)
-#            
-#            # Grab corner of card image, zoom, and threshold
-#            corner = warp[60:570, 0:630]
-#            
-#            if debug_pics: cv2.imwrite(debug_path + "2_corner.jpg",corner)
-#        
-#            corner_zoom = cv2.resize(corner, (0,0), fx=4, fy=4)
-#            corner_blur = cv2.GaussianBlur(corner_zoom,(5,5),0)
-#            
-#            if debug_pics: cv2.imwrite(debug_path + "3_after_blur.jpg",corner_blur)
         
             dummy, cnts, hier = cv2.findContours(warp, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
             cnts = sorted(cnts, key=cv2.contourArea,reverse=True)
