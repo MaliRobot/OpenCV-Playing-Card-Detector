c2a38cebf1cbfc8f8c5c5b9616e1e5356e2ac864
Milos Trifunovic <MaliRobot@users.noreply.github.com>
2018-03-12 21:33:44 +0100

    Update Cards.py

--- Cards.py
+++ Cards.py
@@ -105,10 +105,6 @@ def preprocess_image(image):
     img_w, img_h = np.shape(image)[:2]
     bkg_level = gray[int(img_h/25)][int(img_w/2)]
     thresh_level = bkg_level + BKG_THRESH
-    
-    # try this for white border card
-#    thresh_level = 100
-    #
 
     retval, thresh = cv2.threshold(blur,thresh_level,255,cv2.THRESH_BINARY)
     
@@ -123,7 +119,6 @@ def preprocess_white_image(image):
     bkg_level = gray[int(img_h/25)][int(img_w/2)]
     thresh_level = bkg_level + BKG_THRESH
 
-#    if thresh_level < 100:
     thresh_level = 110
     retval, thresh = cv2.threshold(blur,thresh_level,255,cv2.THRESH_BINARY)
     
@@ -138,10 +133,8 @@ def find_cards(thresh_image, thresh_image_white):
     dummy,cnts_wht,hier_wht = cv2.findContours(thresh_image_white,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     index_sort = sorted(range(len(cnts)), key=lambda i : cv2.contourArea(cnts[i]),reverse=True)
-#    index_sort = [x for x in index_sort if cv2.contourArea(cnts[x]) > 50000]
     
     index_sort_wht = sorted(range(len(cnts_wht)), key=lambda i : cv2.contourArea(cnts_wht[i]),reverse=True)
-#    index_sort_wht = [x for x in index_sort_wht if cv2.contourArea(cnts_wht[x]) > 50000]
 
     # If there are no contours, do nothing
     if len(cnts) == 0 and len(cnts_wht) == 0:
@@ -235,13 +228,8 @@ def match_card(qCard, train_ranks):
     """Finds best rank matches for the query card. Differences
     the query card rank images with the train rank images.
     The best match is the rank image that has the least difference."""
-<<<<<<< HEAD
-    RANK_DIFF_MAX = 6000000
-    best_rank_match_diff = 20000000
-=======
     
     best_rank_match_diff = 30000000
->>>>>>> ccorcorner
     best_rank_match_name = "Unknown"
     best_rank_name = None
     
@@ -278,25 +266,14 @@ def match_card(qCard, train_ranks):
 
             print(score)
             if score < best_rank_match_diff and score > 0:
-#                print(score, best_rank_match_diff)
                 best_rank_match_diff = score
                 best_rank_name = Trank.name
-
-
-
-#            diff_img = cv2.absdiff(qCard.rank_img, Trank.img)
-#
-#            rank_diff = int(np.sum(diff_img)/255)
-##            print(rank_diff)
-#            if rank_diff < best_rank_match_diff:
-#                best_rank_match_diff = rank_diff
-#                best_rank_name = Trank.name
 
     # Combine best rank match and best suit match to get query card's identity.
     # If the best matches have too high of a difference value, card identity
     # is still Unknown
 #    if (best_rank_match_diff < RANK_DIFF_MAX):
-        best_rank_match_name = best_rank_name
+    best_rank_match_name = best_rank_name
 
     # Return the identiy of the card and the quality of the suit and rank match
     return best_rank_match_name, best_rank_match_diff
